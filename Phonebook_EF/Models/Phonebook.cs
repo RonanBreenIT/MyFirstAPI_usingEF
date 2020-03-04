bb48a1e73e4e707ef7a4a2874b29fcbbea90d076
Ronan Breen <X00152190@mytudublin.ie>
2020-03-04 18:40:09 +0000

    Added Home page (Controller), Living doc and comments so users will understand the API actions and DB set up.

--- Phonebook_EF/Models/Phonebook.cs
+++ Phonebook_EF/Models/Phonebook.cs
@@ -6,20 +6,34 @@ using System.Web;
 
 namespace MyFirstAPI.Models
 {
+    /// <summary>
+    /// Holds individual details for Phonebook Entries. 
+    /// </summary>
     public class Phonebook
     {
+        /// <summary>
+        /// The Primary Key ID for Phonebook Entry
+        /// </summary>
         [Key]
         public int ID { get; set; }
 
+        /// <summary>
+        /// The Name for Phonebook Entry. Cannot be null. 
+        /// </summary>
         [Required]
         public String Name { get; set; }
 
+        /// <summary>
+        /// The Number for Phonebook Entry. Nullable. 1 - 20 charachters.
+        /// </summary>
         [Required]
         [MinLength(1)]
         [MaxLength(20)]
         public String Number { get; set; }
 
-        
+        /// <summary>
+        /// The Number for Phonebook Entry. Nullable. 
+        /// </summary>
         public String Address { get; set; }
 
         public Phonebook()
